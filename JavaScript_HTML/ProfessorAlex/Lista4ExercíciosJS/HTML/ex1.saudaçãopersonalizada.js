008af3bbce8783ac7b2c36c0091a634e8300c891
godoyzeraaa <godoyzeraaa@icloud.com>
2025-04-28 21:06:51 -0300

    teste

---
+++ JavaScript_HTML/ProfessorAlex/Lista4ExercíciosJS/HTML/ex1.saudaçãopersonalizada.js
@@ -0,0 +1,7 @@
+function nome(){
+let nome = prompt("digite seu nome:")
+
+alert (`ola ${nome},seja bem vindo`)
+
+
+}
---
+++ JavaScript_HTML/ProfessorAlex/lista2Exer3./exer3.js
@@ -0,0 +1,13 @@
+function exercicios3lista(){
+    let nome = prompt("digite seu nome para saber o seu nome de duper heroi: ")
+    let min = 1
+    let max = 3
+    let numeroaleatorio = Math.floor(Math.random() * ( max - min) + min)
+
+    // math.ceil()-> arredonda para cima
+    //math.floor()-> arredonda pra baixo
+    //math.round()-> arredonda pro mais proximo
+let frase1 = "capitão america"
+let frase2 = "homem aranha"
+let frase3 = "homem de ferro"
+}
