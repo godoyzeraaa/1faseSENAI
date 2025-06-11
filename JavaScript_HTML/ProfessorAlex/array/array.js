c14019273ea5f3d107038b8cfa46c9dbabcaed3d
godoyzeraaa <godoyzeraaa@icloud.com>
2025-06-11 15:01:01 -0300

    arrays
    arrays

---
+++ JavaScript_HTML/ProfessorAlex/array/array.js
@@ -0,0 +1,26 @@
+// let nomes = ["joao","clara", "jonas", "maria", "alesssandro"]
+// //console.log(nomes[10])
+// //console.log(neoms.length)
+// nomes.forEach((element, index) =>)
+// {
+//     console.log(element + "@globomail.com")
+// }
+// nomesComSobrenome = nomes.map((nome) =>)
+// nomes [4] = "alex"
+ 
+// console.log(nomes)
+let numeros = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+//12
+let numeroDois = numeros.map(elemento *2)
+console.log(numeros)
+console.log(numeroDois)
+//14
+let soma = numeros.reduce ((acumuldado, valoratual)=> acumulado+valoratual)
+console.log(soma)
+
+let nomes2 = ["joao","clara","jonas", "maria", "alessandro", "carlos", "jose"]
+let nomeVirgula = nomes2.reduce(( acumulado, valoratual) => acumulado + "," + valoratual)
+console.log(nomeVirgula)
+
+let numeros = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+let numerosInvertidos = numerodois.map((elemento, index)=>)
